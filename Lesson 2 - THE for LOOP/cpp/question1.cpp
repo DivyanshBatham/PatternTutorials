28a0c18d87d038dfb695913727d52f6dc8300586
Divyansh Batham <divyansh.sunita@gmail.com>
2019-03-25 20:00:10 +0530

    Lesson 2

---
+++ Lesson 2 - THE for LOOP/cpp/question1.cpp
@@ -0,0 +1,18 @@
+/*
+ * Question 1 – WAP to loop p from 1 to 15.
+ * 
+ * Written By - Divyansh Batham
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int p = 1; p <= 15; p++)
+    {
+        cout << p << endl;
+    }
+    return 0;
+}
